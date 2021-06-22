dc04219760fc79f94e5136dbb53b47e3b405c7d1
karamaru <38310693+karamaru-alpha@users.noreply.github.com>
2021-06-22 19:24:59 +0900

    feat: CloudSQL設定追加 (#4)
    * change: instance変数名変更
    
    * feat: database作成
    
    * feat: rootユーザ作成
    
    Co-authored-by: 唐木 稜生 <s14044@ca-20013110.cyberagent.co.jp>

--- cloud_sql.tf
+++ cloud_sql.tf
@@ -1,4 +1,4 @@
-resource "google_sql_database_instance" "cooking_bomb" {
+resource "google_sql_database_instance" "cookingbomb" {
   name             = "cookingbomb-mysql"
   database_version = "MYSQL_8_0"
   region           = "us-west1"
@@ -6,4 +6,18 @@ resource "google_sql_database_instance" "cooking_bomb" {
   settings {
     tier = "db-f1-micro"
   }
+}
+
+resource "google_sql_database" "cookingbomb_database" {
+  name      = "cooking_bomb"
+  instance  = google_sql_database_instance.cookingbomb.name
+  charset   = "utf8mb4"
+  collation = "utf8mb4_bin"
+}
+
+resource "google_sql_user" "cookingbomb_root" {
+  name     = "root"
+  instance = google_sql_database_instance.cookingbomb.name
+  host     = "%"
+  password = var.MYSQL_ROOT_PASSWORD
 }
