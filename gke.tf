0ba677c6dd0fff62854d4ab0f1f09a52f65eb2a3
Karacky <mrnk3078@gmail.com>
2021-08-26 18:28:48 +0900

    戻す

--- gke.tf
+++ gke.tf
@@ -1,6 +1,7 @@
-resource "google_container_cluster" "primary" {
-  name     = "cookingbomb-cluster"
-  location = "us-west1"
+# 節約のために一旦置いとく
+# resource "google_container_cluster" "primary" {
+#   name     = "cookingbomb-cluster"
+#   location = "us-west1"
 
-  enable_autopilot = true
-}
+#   enable_autopilot = true
+# }
