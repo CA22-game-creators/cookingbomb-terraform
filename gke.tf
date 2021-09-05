d5b94a0b1067a6d67574ef4115d2f5fdc501dc3f
Karacky <mrnk3078@gmail.com>
2021-09-05 13:33:37 +0900

    gke 有効化

--- gke.tf
+++ gke.tf
@@ -1,6 +1,6 @@
-# resource "google_container_cluster" "primary" {
-#   name     = "cookingbomb-cluster"
-#   location = "us-west1"
-
-#   enable_autopilot = true
-# }
+resource "google_container_cluster" "primary" {
+  name             = "cookingbomb-cluster"
+  location         = "us-west1"
+  enable_autopilot = true
+  tags             = ["game-server"]
+}
